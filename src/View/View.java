e5df77d83fac3d0cfb065eefec6ba4b8814f95ce
nelly-lam <49532469+nelly-lam@users.noreply.github.com>
2021-09-22 16:02:18 +0200

    update View

--- src/View/View.java
+++ src/View/View.java
@@ -13,6 +13,7 @@ public class View extends Application {
     public void start(Stage primaryStage) throws Exception {
         Pane start = (Pane) FXMLLoader.load(getClass().getResource("../acceuil.fxml"));
         Scene welcome = new Scene(start,start.getPrefWidth(),start.getPrefHeight());
+        welcome.getRoot().requestFocus();
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Virus Game");

6047369fb3df05cce5cb0300565e7f4a10b8a543
charlottetrupin <59920560+charlottetrupin@users.noreply.github.com>
2021-09-22 12:12:26 +0200

    first and main page

--- src/Controller/ControllerAcceuil.java
+++ src/Controller/ControllerAcceuil.java
@@ -1,62 +1,49 @@
 package Controller;
 
+import com.sun.source.doctree.AttributeTree;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.awt.*;
+import java.io.IOException;
 
-public class ControllerAcceuil {
+public class ControllerAcceuil  {
 
-    @FXML private Button start;
-    @FXML private Button exit;
-    private Boolean close = false;
+    @FXML Button button;
 
-
-
-
-    @FXML
-    protected void Start(KeyEvent e) throws Exception{
-           if(e.getCode().equals(KeyCode.DOWN)){
-                start.setStyle("-fx-border-color:black");
-                start.setStyle("-fx-background-color:black");
-                close = true;
-        }
-           else
-            System.out.println("hello");
-
-
-
-        //System.out.println("start pressed");
-    //    Stage primaryStage = (Stage) Start.getScene().getWindow();
-       // FXMLLoader loader = new FXMLLoader(getClass().getResource("../Fxml/dimension.fxml"));
-       // Pane myPane = loader.load();
-       // Controller c = loader.getController();
-       // c.setLongueur(200);
-       // c.setLarg(200);
-       // Scene myScene = new Scene(myPane, myPane.getPrefWidth()*1.5,myPane.getPrefHeight()*1.5);
-        //primaryStage.setScene(myScene);
-        //primaryStage.setX(primaryStage.getX()-250);
-        //primaryStage.setY(primaryStage.getY()-75);
-        //primaryStage.show();
-    }
+/*
+       Stage primaryStage = (Stage) acceuil.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Fxml/dimension.fxml"));
+        Pane myPane = loader.load();
+        Controller c = loader.getController();
+        c.setLongueur(200);
+        c.setLarg(200);
+        Scene myScene = new Scene(myPane, myPane.getPrefWidth()*1.5,myPane.getPrefHeight()*1.5);
+        primaryStage.setScene(myScene);
+        primaryStage.setX(primaryStage.getX()-250);
+        primaryStage.setY(primaryStage.getY()-75);
+        primaryStage.show();*/
 
     @FXML
-    protected void Exit() throws Exception{
-        if(close) {
-            exit.setStyle("-fx-border-color: yellow; -fx-background-color:black");
-            //exit.setStyle("-fx-background-color:black");
-            Stage primaryStage = (Stage) exit.getScene().getWindow();
-            System.out.println("here");
+    public void here(KeyEvent keyEvent) throws IOException {
+        Stage primaryStage = (Stage) button.getScene().getWindow();
+        if(keyEvent.getCode().equals(KeyCode.SPACE)){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../game.fxml"));
+            Pane myPane = loader.load();
+            //Controller c = loader.getController();
+            Scene myScene = new Scene(myPane, myPane.getPrefWidth(),myPane.getPrefHeight());
+            primaryStage.setScene(myScene);
+            primaryStage.show();
         }
-       // primaryStage.close();
+        else if(keyEvent.getCode().equals(KeyCode.Q)){
+            primaryStage.close();
+        }
     }
 }
